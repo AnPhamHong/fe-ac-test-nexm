9ee5fe0a6f60821abc1f4df9deb33a5e844a2168
An Pham Hong <an.pham@apps-cyclone.com>
2023-12-21 01:15:42 +0700

    build: read file txt

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("init log");
+var fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const requestQuestion = () => {
+  rl.question("Please enter your selection (help | quit): ", (input) => {
+    if (input === "quit") {
+      rl.close();
+    }
+    if (input === "help") {
+      fs.readFile("help.txt", "utf8", function (err, data) {
+        // Display the file content
+        if (err) {
+          console.log("Read file error: ", err);
+        }
+        console.log(data);
+        requestQuestion();
+      });
+      return;
+    }
+  });
+};
+
+//EXECUTE MAIN
+requestQuestion();
